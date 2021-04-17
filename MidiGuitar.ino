fb13fa97e900242d0c398190bad156b4b8a8723b
One Sad Puppy <j@onesadpuppy.com>
2021-04-16 21:54:25 -0400

    switching workstations

--- MidiGuitar.ino
+++ MidiGuitar.ino
@@ -11,9 +11,8 @@ void setup()
 {
 
   /*
-  The following 3 lines of code are borrowed from microsmart.co.za/technical/2013/03/01/advanced-arduino-adc/
-   They set the Analog to Digital Converter prescale clock to 16, meaning 16Mhz / prescale(16) = 1Mhz
-   The adc requires 25 cycles to resolve a value so 1Mhz/25 = approx 40,000 samples per second
+   The following 3 lines set the Analog to Digital Converter prescale clock to 16
+   This should allow for analog read rates at ~77 Khz. 
    */
   sbi(ADCSRA,ADPS2) ;
   cbi(ADCSRA,ADPS1) ;
@@ -21,8 +20,8 @@ void setup()
 
   // initializes the serial communication, 31250 is the MIDI spec baud rate
   Serial.begin(31250);
-
-void loop() //like java's main, but more powerful, this body loops indefinitely
+}
+void loop()
 {
 
 
@@ -34,3 +33,25 @@ void fretChecker()
 
 }
 
+void midiPlay(int cmd, int note, int velocity)
+{
+/*
+	midiPlay(0x99,pad[i].noteValue,vel); //hex 99 is note on, second nine is channel 10
+	midiPlay(0x99,pad[i].noteValue,0x00); // velocity of 0 indicates note off
+	also 0x8c where c is channel is valid note off. zero velocity might be easier however.
+	
+	pitch bend is 0xEc byte 1 is bend LSB, byte 2 is MSB
+	each byte in this case is 7 bits. 127 max for each
+	for my purposes it might make sense to only read the MSB, or modulate it at least. will need to do some mathi
+
+	pitch bend 0 - 16383
+	b1 is msb
+	b2 is lsb
+	p = b1 << 7 + b2
+
+	midi note 60 is middle C	
+*/
+  Serial.write(cmd);
+  Serial.write(note);
+  Serial.write(velocity); 
+}
